35bb4b184002b52f06b4a243c73461324db5fe61
Kamogelo Jabulile Motlhale <KAMOGELOMO@SANLAMSKY.CO.ZA>
2024-03-19 13:52:53 +0200

    fixes

--- models/review.py
+++ models/review.py
@@ -7,7 +7,12 @@ from sqlalchemy import Column, String, ForeignKey
 
 class Review(BaseModel, Base):
     """ Review classto store review information """
-    __tablename__ = "reviews"
-    text = Column(String(1024), nullable=False)
-    place_id = Column(String(60), ForeignKey("places.id"), nullable=False)
-    user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
+    if models.is_type == 'db':
+        __tablename__ = "reviews"
+        text = Column(String(1024), nullable=False)
+        place_id = Column(String(60), ForeignKey("places.id"), nullable=False)
+        user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
+    else:
+        place_id = ""
+        user_id = ""
+        text = ""
