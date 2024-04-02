5bcba1ada980b449a473511993e05ad2a6e27c76
brikhotso <brenda.rikhotso@gmail.com>
2024-04-02 12:35:48 -0700

    test city

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -2,6 +2,7 @@
 """ """
 from tests.test_models.test_base_model import test_basemodel
 from models.city import City
+import os
 
 
 class test_City(test_basemodel):
@@ -15,10 +16,14 @@ class test_City(test_basemodel):
 
     def test_state_id(self):
         """ """
-        new = self.value()
-        self.assertEqual(type(new.state_id), str)
+        new = self.value(state_id="TestState")
+        self.assertEqual(type(new.state_id), str if
+                         os.getenv('HBNB_TYPE_STORAGE') != 'db' else
+                         type(None))
 
     def test_name(self):
         """ """
-        new = self.value()
-        self.assertEqual(type(new.name), str)
+        new = self.value(name="TestCity")
+        self.assertEqual(type(new.name), str if
+                         os.getenv('HBNB_TYPE_STORAGE') != 'db' else
+                         type(None))
