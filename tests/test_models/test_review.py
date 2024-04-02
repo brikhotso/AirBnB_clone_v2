0307d8a0edb0c319b440cb884f33fdfcbf3ad5e0
brikhotso <brenda.rikhotso@gmail.com>
2024-04-02 12:37:56 -0700

    test review

--- tests/test_models/test_review.py
+++ tests/test_models/test_review.py
@@ -2,6 +2,7 @@
 """ """
 from tests.test_models.test_base_model import test_basemodel
 from models.review import Review
+import os
 
 
 class test_review(test_basemodel):
@@ -15,15 +16,21 @@ class test_review(test_basemodel):
 
     def test_place_id(self):
         """ """
-        new = self.value()
-        self.assertEqual(type(new.place_id), str)
+        new = self.value(place_id="test_place_id")
+        self.assertEqual(type(new.place_id), str if
+                         os.getenv('HBNB_TYPE_STORAGE') != 'db' else
+                         type(None))
 
     def test_user_id(self):
         """ """
-        new = self.value()
-        self.assertEqual(type(new.user_id), str)
+        new = self.value(user_id="test_user_id")
+        self.assertEqual(type(new.user_id), str if
+                         os.getenv('HBNB_TYPE_STORAGE') != 'db' else
+                         type(None))
 
     def test_text(self):
         """ """
-        new = self.value()
-        self.assertEqual(type(new.text), str)
+        new = self.value(text="This is a test review.")
+        self.assertEqual(type(new.text), str if
+                         os.getenv('HBNB_TYPE_STORAGE') != 'db' else
+                         type(None))
