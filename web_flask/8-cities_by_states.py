66fdd94b70bdbb685c5a5b7fc94842d072c2731c
brikhotso <brenda.rikhotso@gmail.com>
2024-04-21 14:32:56 -0700

    declare class properly

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,31 +1,23 @@
 #!/usr/bin/python3
-"""Starts a Flask web application.
-
-The application listens on 0.0.0.0, port 5000.
-Routes:
-    /states_list: HTML page with a list of all State objects in DBStorage.
-"""
+""" Script that starts a Flask web application """
 from flask import Flask, render_template
 from models import storage
-
+from models.state import State
 
 app = Flask(__name__)
 
 
-@app.route("/cities_by_states", strict_slashes=False)
-def cities_by_states():
-    """Displays an HTML page with a list of all states and related cities.
-
-    States/cities are sorted by name.
-    """
-    states = storage.all("State")
-    return render_template("8-cities_by_states.html", states=states)
-
-
 @app.teardown_appcontext
-def teardown(exc):
-    """Remove the current SQLAlchemy session."""
+def teardown_db(exception):
+    """Remove the current SQLAlchemy Session."""
     storage.close()
+
+
+@app.route('/cities_by_states', strict_slashes=False)
+def cities_by_states():
+    """Display a HTML page with states and their cities."""
+    states = storage.all(State).values()
+    return render_template('8-cities_by_states.html', states=states)
 
 
 if __name__ == '__main__':
