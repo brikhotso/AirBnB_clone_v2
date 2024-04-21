42e4b5be095b9aa9bb6ba9d1e7e2554b4e0d686f
brikhotso <brenda.rikhotso@gmail.com>
2024-04-21 15:14:04 -0700

    states

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -6,37 +6,39 @@ Routes:
     /states: HTML page with a list of all State objects.
     /states/<id>: HTML page displaying the given state with <id>.
 """
+from flask import Flask, request, render_template
 from models import storage
-from flask import Flask
-from flask import render_template
+from models.city import City
+from models.state import State
+
 
 app = Flask(__name__)
 
 
 @app.route("/states", strict_slashes=False)
-def states():
-    """Displays an HTML page with a list of all States.
-
-    States are sorted by name.
-    """
-    states = storage.all("State")
-    return render_template("9-states.html", state=states)
-
-
 @app.route("/states/<id>", strict_slashes=False)
-def states_id(id):
-    """Displays an HTML page with info about <id>, if it exists."""
-    for state in storage.all("State").values():
-        if state.id == id:
-            return render_template("9-states.html", state=state)
-    return render_template("9-states.html")
+def states_id(id=None):
+    """ Renders all states or a specific state based on the given ID """
+    states = storage.all(State)
+    if id is None:
+        return render_template(
+            "9-states.html", states=states
+        )
+    else:
+        return render_template(
+            "9-states.html", states=states,
+            id="State." + id
+        )
 
 
 @app.teardown_appcontext
-def teardown(exc):
-    """Remove the current SQLAlchemy session."""
+def remove_session(exception):
+    """
+    Closes the storage session at the end of the app context,
+    regardless of exceptions
+    """
     storage.close()
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
