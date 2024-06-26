e85ca8352d5362cf391a97d794f3f07d7dcb5fc5
brikhotso <brenda.rikhotso@gmail.com>
2024-06-26 09:07:15 +0000

    backup

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -2,14 +2,14 @@
 """
 script starts Flask web app
     listen on 0.0.0.0, port 5000
-    routes: /: display "Hello HBNB!"
+    routes: /airbnb-onepage/: display "Hello HBNB!"
 """
 from flask import Flask
 
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
+@app.route('/airbnb-onepage/', strict_slashes=False)
 def hello_hbnb():
     return 'Hello HBNB!'
 
